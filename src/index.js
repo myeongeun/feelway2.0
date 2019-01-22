60b2d7279b1b1777c9da3d6ca709c9fb254258db
김종호 <kjh@stunitas.com>
2019-01-22 16:45:15 +0900

    bootstrap fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 import _ from 'lodash';
-import '../node_modules/bootstrap';
 import './application.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,10 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
         use: ['file-loader'],
       },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: 'file-loader',
+      },
     ],
   },
   entry: {
