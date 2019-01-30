d774203ca648dbc4e0907152afb11eb56ab93ab9
김종호 <kjh@stunitas.com>
2019-01-31 00:03:29 +0900

    ver-0.1.3: update(CHART: charts)

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // Common
 import _ from 'lodash';
 // import 'chosen-js';
-// import 'c3';
-import 'd3';
-import 'c3';
 import './src/js/bootstrap';
 import './src/application.scss';
 import 'jquery';
 // import './plugins/jquery.steps/jquery-steps';
 // import 'jquery-validation';
+import 'd3';
+import 'c3';
 import './src/js/custom';
 
 // Entry
