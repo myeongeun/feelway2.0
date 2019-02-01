68179876979e2af5f7b579680a23ba725fbacf1d
김종호 <kjh@stunitas.com>
2019-02-01 12:12:08 +0900

    ver-0.1.4: update(plugins add:)

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 // Common
 import _ from 'lodash';
-// import 'chosen-js';
 import './src/js/bootstrap';
 import './src/application.scss';
-import 'jquery';
-// import './plugins/jquery.steps/jquery-steps';
-// import 'jquery-validation';
-import 'd3';
-import 'c3';
+import 'jquery/dist/jquery';
+import 'jquery-steps/build/jquery.steps.min';
+import 'jquery-steps/lib/jquery.cookie-1.3.1';
+import './plugins/jquery.steps/jquery.steps';
+import 'jquery-validation';
+import 'chosen-js/chosen.jquery';
+import 'd3/dist/d3';
+import 'c3/c3';
 import './src/js/custom';
+
+import 'chosen-js';
+import 'ion-rangeslider';
+import 'select2';
+import 'nouislider';
+import 'bootstrap-datepicker/dist/js/bootstrap-datepicker';
+import 'clockpicker/dist/bootstrap-clockpicker';
+import 'bootstrap-daterangepicker';
+import 'bootstrap-tagsinput';
+import 'imagecropper/dist/js/cropper';
 
 // Entry
 import './main';
