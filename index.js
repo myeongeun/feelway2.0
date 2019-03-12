a58ca91d33cc9c231b4269082cd25e816d34828f
김종호 <kjh@stunitas.com>
2019-03-12 20:27:58 +0900

    ver-0.2.0: update(plugins, views)

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 // Common
 import _ from 'lodash';
 import $ from 'jquery';
-import 'jquery-ui';
-import 'moment';
-import 'bootstrap/dist/js/bootstrap';
-import 'font-awesome-loader';
-import './plugins/jquery.steps/jquery.steps';
-import 'jquery-steps/build/jquery.steps.min';
-import 'jquery-steps/lib/jquery.cookie-1.3.1';
-import 'jquery-validation';
-import './src/js/custom';
+import jqueryui from 'jquery-ui';
+import moment from 'moment';
+import bootstrap from 'bootstrap/dist/js/bootstrap';
+import fontawesomeloader from 'font-awesome-loader';
+import jquerysteps from './plugins/jquery.steps/jquery.steps';
+import jquerycookie from 'jquery-steps/lib/jquery.cookie-1.3.1';
+import jqueryvalidation from 'jquery-validation';
+
+// Plugins
+import chosen from 'chosen-js/chosen.jquery';
+import d3 from 'd3/dist/d3';
+import c3 from 'c3/c3';
+import rangeslider from 'ion-rangeslider';
+import select2 from 'select2';
+import uislider from 'nouislider';
+import datepicker from 'bootstrap-datepicker/dist/js/bootstrap-datepicker';
+import jqeurycloCkpicker from 'clockpicker/dist/jquery-clockpicker';
+import bootstrapClockpicker from 'clockpicker/dist/bootstrap-clockpicker.min';
+import cropper from 'imagecropper/dist/js/cropper';
+
+import custom from './src/js/custom';
 
 // Sass
 import './src/application.scss';
@@ -21,3 +33,36 @@ import './layouts';
 import './plugins';
 import './helper';
 import './guide';
+
+jqueryui();
+moment();
+bootstrap();
+fontawesomeloader();
+jquerysteps();
+jquerycookie();
+jqueryvalidation();
+
+chosen();
+d3();
+c3();
+rangeslider();
+select2();
+uislider();
+datepicker();
+jqeurycloCkpicker();
+bootstrapClockpicker();
+cropper();
+
+custom();
+
+$('#myPicker input').datepicker();
+
+$('#component-group .input-group.date').datepicker({
+  todayHighlight: true
+});
+
+$('#range-group .input-daterange').datepicker({
+  daysOfWeekHighlighted: "0,6",
+  todayHighlight: true,
+  toggleActive: true
+});
